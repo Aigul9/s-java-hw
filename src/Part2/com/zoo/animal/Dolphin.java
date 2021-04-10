be9550609477ed6a215f2410ea05faf8d35ffc0e
Aigul <aigul.sibgatulli@mail.ru>
2021-04-10 12:31:28 +0300

    hw inheritance

--- src/Part2/com/zoo/animal/Dolphin.java
+++ src/Part2/com/zoo/animal/Dolphin.java
@@ -11,7 +11,9 @@ public class Dolphin extends Animal {
     }
 
     @Override
-    public void eat() { System.out.println("Dolphin is eating."); }
+    public void eat() {
+        System.out.println("Dolphin is eating.");
+    }
 
     @Override
     public void sleep() {
